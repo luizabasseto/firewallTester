901b734d5a2882a4c30c9cde02715061da1876b8
luizabasseto <luizabasseto.1@gmail.com>
2026-04-23 20:10:27 -0300

    fix: readme and added container to test

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Main entry point for the Firewall Tester application.
 This script initializes the PyQt5 application, creates the main window,
 and starts the event loop.
 """
-"""Para rodar a aplicação faça o comando pip3 install -r requirements.txt na VM"""
+"""To run the application, execute the command `pip3 install -r requirements.txt` on the terminal before execute the software"""
 
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.main_ui import MainWindow
+import docker
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
